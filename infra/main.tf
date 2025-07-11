3065a76da0b5aea62cc31c13230c888dc14de40d
unknown <kaiyi.huang@yitu-inc.com>
2025-07-11 16:44:32 +0400

    public ip

--- infra/main.tf
+++ infra/main.tf
@@ -78,6 +78,8 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  
+  associate_public_ip_address = true 
 
   tags = {
     Name = "devops-web-instance"
