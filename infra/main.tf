46248b87bf01f59905659513e907eb89f173343a
unknown <kaiyi.huang@yitu-inc.com>
2025-07-11 17:29:55 +0400

    add cloudwatch

--- infra/main.tf
+++ infra/main.tf
@@ -79,7 +79,49 @@ resource "aws_instance" "web" {
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   
-  associate_public_ip_address = true 
+  associate_public_ip_address = true
+  
+  iam_instance_profile = aws_iam_instance_profile.cloudwatch_profile.name
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y amazon-cloudwatch-agent
+
+              cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<EOL
+              {
+                "agent": {
+                  "metrics_collection_interval": 60,
+                  "run_as_user": "root"
+                },
+                "metrics": {
+                  "append_dimensions": {
+                    "InstanceId": "\\${aws:InstanceId}"
+                  },
+                  "metrics_collected": {
+                    "cpu": {
+                      "measurement": [
+                        "cpu_usage_idle",
+                        "cpu_usage_iowait"
+                      ],
+                      "metrics_collection_interval": 60
+                    },
+                    "mem": {
+                      "measurement": [
+                        "mem_used_percent"
+                      ]
+                    }
+                  }
+                }
+              }
+              EOL
+
+              /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+                -a fetch-config \
+                -m ec2 \
+                -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+                -s
+              EOF
 
   tags = {
     Name = "devops-web-instance"
@@ -88,10 +130,40 @@ resource "aws_instance" "web" {
 
 resource "aws_s3_bucket" "mybucket" {
   bucket = "kaiyihuang-devops-bucket-202507-new" # 全局唯一名字，自行改
-  acl    = "private"
+  acl = "private"
   
   tags = {
     Name        = "devops-s3-bucket"
     Environment = "dev"
   }
+}
+
+resource "aws_iam_role" "ec2_cloudwatch_role" {
+  name = "ec2-cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
+  role       = aws_iam_role.ec2_cloudwatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_cloudwatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "cloudwatch_profile" {
+  name = "cloudwatch-instance-profile"
+  role = aws_iam_role.ec2_cloudwatch_role.name
 }
